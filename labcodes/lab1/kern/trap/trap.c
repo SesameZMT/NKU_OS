94de627ffbecc500f3f8558fb4ac9a3913f4f206
sesame <1930959782@qq.com>
2023-09-23 14:31:01 +0800

    Lab1 EXERCISE2

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -106,12 +106,21 @@ void interrupt_handler(struct trapframe *tf) {
             // In fact, Call sbi_set_timer will clear STIP, or you can clear it
             // directly.
             // cprintf("Supervisor timer interrupt\n");
-             /* LAB1 EXERCISE2   YOUR CODE :  */
+             /* LAB1 EXERCISE2   YOUR CODE : 2111454 */
             /*(1)设置下次时钟中断- clock_set_next_event()
              *(2)计数器（ticks）加一
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
+            clock_set_next_event();
+            if(ticks++ % TICK_NUM == 0 && num < 10)
+            {
+                num++;
+                print_ticks();
+            }
+            else if(num == 10){
+                sbi_shutdown();
+            }
             break;
         case IRQ_H_TIMER:
             cprintf("Hypervisor software interrupt\n");
