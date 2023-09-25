43ab57860ee64fd570d2e273513d25b9efb646e0
sesame <1930959782@qq.com>
2023-09-25 17:06:21 +0800

    add lab1

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -112,7 +112,8 @@ void interrupt_handler(struct trapframe *tf) {
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
-            clock_set_next_event();
+
+            clock_set_next_event();// 定义在/kern/driver/clock.c
             if(ticks++ % TICK_NUM == 0 && num < 10)
             {
                 num++;
