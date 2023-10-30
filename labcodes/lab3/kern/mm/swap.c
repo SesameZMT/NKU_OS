f380854b57ea17e77d12ec20c2a9b5a0068cd029
“sesame” <1930959782@qq.com>
2023-10-30 08:26:04 +0800

    lab3 challenge

--- labcodes/lab3/kern/mm/swap.c
+++ labcodes/lab3/kern/mm/swap.c
@@ -2,6 +2,7 @@
 #include <swapfs.h>
 #include <swap_fifo.h>
 #include <swap_clock.h>
+#include <swap_lru.h>
 #include <stdio.h>
 #include <string.h>
 #include <memlayout.h>
@@ -39,7 +40,7 @@ swap_init(void)
         panic("bad max_swap_offset %08x.\n", max_swap_offset);
      }
 
-     sm = &swap_manager_clock;//use first in first out Page Replacement Algorithm
+     sm = &swap_manager_lru;//use first in first out Page Replacement Algorithm
      int r = sm->init();
      
      if (r == 0)
